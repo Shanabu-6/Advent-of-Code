83d6a1ea78284ad4420ad640cf904240e3e842d8
Shanabu-6 <60versets_mentholes@icloud.com>
2025-12-03 18:22:58 +0100

    maj 2015

--- 2015/02/day2.1_2015.py
+++ 2015/02/day2.1_2015.py
@@ -1,11 +1,32 @@
-l = 1
-w = 1
-h = 10
-extra = 0
+filename = 'list_presents.csv'
 
-liste = ()
+total_surface = 0
 
-surface_box = 2*l*w + 2*w*h + 2*h*l + extra
+## surface of the box ##
 
-print(surface_box)
 
+
+
+with open(filename) as f:      #open csv
+	for ligne in f:
+		ligne = ligne.strip()
+		fields = ligne.split('x') #clean the data : from aaxbbxcc to 'aa' 'bb' 'cc', for each line
+		numbers = [int(i) for i in fields] # str -> int
+		
+		
+		l = numbers[0] #assigne position à chaque int
+		w = numbers[1]
+		h = numbers[2]
+
+## extra ##
+		
+		side1 = l*w 
+		side2 = w*h 
+		side3 = h*l
+		extra = min(side1, side2, side3) #extra face = shortest side
+			
+		surface_box = 2*side1 + 2*side2 + 2*side3 + extra
+
+		total_surface = total_surface + surface_box
+
+		print(total_surface)
